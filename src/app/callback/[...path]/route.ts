818abd9a66e1340d4ddcc988227e7c1a4a6844dd
talisk21 <talisk@i.ua>
2024-12-03 12:03:51 +0200

    remove extra logging

--- src/app/callback/[...path]/route.ts
+++ src/app/callback/[...path]/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
+import axios from "axios";
 
 export async function POST(req: Request, { params }: { params: { path: string[] } }) {
-    const path = `/callback/${params.path.join('/')}`; // Build the full path from params
+    const path = `/callback/${params.path.join('/')}`;
 
-    const loggingApiUrl = 'https://api.wapi.com/API/hs/v1/Services/Shopify/Callback'; // Replace with your logging server URL
+    const loggingApiUrl = 'https://api.wapi.com/API/hs/v1/Services/Shopify/Callback';
 
     // Extract request details
     const url = req.url;
@@ -20,15 +21,15 @@ export async function POST(req: Request, { params }: { params: { path: string[] 
 
     // Log the request
     try {
-        await fetch(loggingApiUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ url, method, path }),
-        });
+        await axios.post(
+            loggingApiUrl,
+            { url, method, path },
+            {headers: { 'Content-Type': 'application/json'}},
+        );
     } catch (error) {
         console.error('Error sending log to logging server:', error);
     }
 
-    // Respond to indicate the route does not exist
-    return NextResponse.json({ status: 200 });
+    // Respond to send success
+    return NextResponse.json({ message: 'Success' }, { status: 200 });
 }
